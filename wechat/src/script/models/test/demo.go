43ca2991ddda8ac1fcd6b970609f1814b24f0e24
liang <liang1024liang@gmail.com>
2026-03-31 17:57:37 +0800

    1234

--- wechat/src/script/models/test/demo.go
+++ wechat/src/script/models/test/demo.go
@@ -4,10 +4,9 @@ import (
 	"comm/comm"
 	"comm/goError"
 	"fmt"
-	jsoniter "github.com/json-iterator/go"
 	"gopkg.in/mgo.v2/bson"
 	info "script/webstru"
-	"selfComm/db/ip"
+	"selfComm/db/sendmsg"
 )
 
 // 群发
@@ -21,28 +20,7 @@ func (this *DemoServer) getUid() string {
 }
 
 func (this *DemoServer) Demo(req1 *info.DemoReq, rsp *info.DemoRsp) *goError.ErrRsp {
-	tmpGroup := &ip.IpGroup{}
-	tmpGroup.Id = bson.NewObjectId()
-	tmpGroup.Name = "测试"
-	ip.AddIpGroup(tmpGroup)
-
-	tmpIp := &ip.Ip{}
-	tmpIp.Id = bson.NewObjectId()
-	tmpIp.GroupId = tmpGroup.Id.Hex()
-	tmpIp.ProxyIp = "173.211.69.97:6690"
-	tmpIp.ProxyUser = "scoohztt"
-	tmpIp.ProxyPwd = "epbs3o70v4iv"
-	tmpIp.ProxyType = "socks5"
-	tmpIp.Status = 1
-	tmpIp.AllotNum = 5
-	tmpIp.IpType = 1
-	tmpIp.IpCategory = 1
-	tmpIp.ExpireStatus = 1
-	tmpIp.Country = "美国"
-	tmpIp.DisableStatus = 1
-	ip.AddIp(tmpIp)
-	lockIp := ip.GetOneLockIp()
-	toString, _ := jsoniter.MarshalToString(lockIp)
-	fmt.Println("测试ip", toString)
+	msgInfo := sendmsg.GetCountSendMsgInfo(bson.M{})
+	fmt.Println(msgInfo)
 	return nil
 }
