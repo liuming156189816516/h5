6f3b76a6499f1ed0e02d166dd0fae7180f036a26
liang <liang1024liang@gmail.com>
2026-03-31 16:11:32 +0800

    1234

--- wechat/src/script/models/test/demo.go
+++ wechat/src/script/models/test/demo.go
@@ -3,7 +3,9 @@ package test
 import (
 	"comm/comm"
 	"comm/goError"
+	"gopkg.in/mgo.v2/bson"
 	info "script/webstru"
+	"selfComm/db/ip"
 )
 
 // 群发
@@ -17,5 +19,25 @@ func (this *DemoServer) getUid() string {
 }
 
 func (this *DemoServer) Demo(req1 *info.DemoReq, rsp *info.DemoRsp) *goError.ErrRsp {
+	tmpGroup := &ip.IpGroup{}
+	tmpGroup.Id = bson.NewObjectId()
+	tmpGroup.Name = "测试"
+	ip.AddIpGroup(tmpGroup)
+
+	tmpIp := &ip.Ip{}
+	tmpIp.Id = bson.NewObjectId()
+	tmpIp.GroupId = tmpGroup.Id.Hex()
+	tmpIp.ProxyIp = "173.211.69.97:6690"
+	tmpIp.ProxyUser = "scoohztt"
+	tmpIp.ProxyPwd = "epbs3o70v4iv"
+	tmpIp.ProxyType = "socks5"
+	tmpIp.Status = 1
+	tmpIp.AllotNum = 5
+	tmpIp.IpType = 1
+	tmpIp.IpCategory = 1
+	tmpIp.ExpireStatus = 1
+	tmpIp.Country = "美国"
+	tmpIp.DisableStatus = 1
+	ip.AddIp(tmpIp)
 	return nil
 }
