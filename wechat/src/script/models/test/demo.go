49bcd774445f2f051de5822c40ca3b542ee18355
lijie5122 <lijie7890abcd.@gmail.com>
2026-04-08 23:05:06 +0800

    xf

--- wechat/src/script/models/test/demo.go
+++ wechat/src/script/models/test/demo.go
@@ -3,8 +3,12 @@ package test
 import (
 	"comm/comm"
 	"comm/goError"
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+	"gopkg.in/mgo.v2/bson"
 	info "script/webstru"
-	"selfComm/wxComm"
+	"selfComm/db/fb"
+	"selfComm/db/log"
 )
 
 // 群发
@@ -17,7 +21,45 @@ func (this *DemoServer) getUid() string {
 	return this.Sess.Uid
 }
 
-func (this *DemoServer) Demo(req1 *info.DemoReq, rsp *info.DemoRsp) *goError.ErrRsp {
-	wxComm.KwaiPlace("iy7RRvunG1KahufyqVEuAg", "EVENT_BUTTON_CLICK")
+func (this *DemoServer) Demo(req *info.DemoReq, rsp *info.DemoRsp) *goError.ErrRsp {
+	//查询上报表，根据手机号去重
+	if req.Type == "1" {
+		listFb := fb.GetListFb(bson.M{}, -1)
+		fmt.Println("执行完成1=========================>", len(listFb))
+		printed := make(map[string]bool)
+		cont := int64(0)
+		for _, fb := range listFb {
+			if fb.Fbclid != "" && !printed[fb.Phone] {
+				printed[fb.Phone] = true
+				toString, _ := jsoniter.MarshalToString(fb)
+				fmt.Println("执行完成1=========================>", toString)
+				cont++
+			}
+		}
+		fmt.Println("执行完成1=========================>", cont)
+	}
+	//小莫上报日志查询
+	if req.Type == "2" {
+		listFb := log.GetListFbReportLog(bson.M{}, -1)
+		fmt.Println("执行完成2=========================>", len(listFb))
+		cont := int64(0)
+		for _, fb := range listFb {
+			toString, _ := jsoniter.MarshalToString(fb)
+			fmt.Println("执行完成2=========================>", toString)
+			cont++
+		}
+		fmt.Println("执行完成2=========================>", cont)
+	}
+
+	//查询给fb上报日志
+	if req.Type == "3" {
+		listFb := log.GetListFbLog(bson.M{}, -1)
+		fmt.Println("执行完成3=========================>", len(listFb))
+		for _, fb := range listFb {
+			toString, _ := jsoniter.MarshalToString(fb)
+			fmt.Println("执行完成3=========================>", toString)
+		}
+	}
+
 	return nil
 }
