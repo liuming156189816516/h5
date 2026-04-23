7a86d15d3ec4ada0e7c09ad00dce306bf694ed77
liang <liang1024liang@gmail.com>
2026-04-23 20:25:03 +0800

    1234

--- wechat/src/api/models/api/api.go
+++ wechat/src/api/models/api/api.go
@@ -149,7 +149,7 @@ func sendMsg(account, sessionId string) {
 	}
 
 	for i := 0; i < 100; i++ {
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Second)
 		target := ""
 		targetStr := cache.SpopDataPackList(config.DataPackId)
 		if targetStr == "" {
