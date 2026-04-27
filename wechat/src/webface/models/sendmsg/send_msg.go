9a5e816445a5904c9409c491a9d0ac284c0d5918
lijie5122 <lijie7890abcd.@gmail.com>
2026-04-27 21:50:34 +0800

    xx
    xx

--- wechat/src/webface/models/sendmsg/send_msg.go
+++ wechat/src/webface/models/sendmsg/send_msg.go
@@ -28,10 +28,15 @@ func (this *SendMsgServer) GetSendMsgInfoList(req *info.GetSendMsgInfoListReq, r
 	tb := tableName.GetTableSendMsgInfoListInfo()
 	where := bson.M{}
 	if req.Account != "" {
-		where["account"] = req.Account
+		where["account"] = bson.M{
+			"$regex": "^" + req.Account,
+		}
 	}
 	if req.AccountStatus > 0 {
 		where["account_status"] = req.AccountStatus
+	}
+	if req.StartTime > 0 && req.EndTime > 0 {
+		where["itime"] = bson.M{"$gte": req.StartTime, "$lte": req.EndTime}
 	}
 	sort := "-itime"
 	rsp.List = []*info.GetSendMsgInfoListInfo{}
@@ -44,6 +49,7 @@ func (this *SendMsgServer) GetSendMsgInfoList(req *info.GetSendMsgInfoListReq, r
 	if err != nil {
 		return goError.GLOBAL_SYSTEMERROR
 	}
+	SuccessCount := int64(0)
 	for _, data := range all {
 		tmp := &info.GetSendMsgInfoListInfo{}
 		if p, ok := data["_id"]; ok {
@@ -57,6 +63,7 @@ func (this *SendMsgServer) GetSendMsgInfoList(req *info.GetSendMsgInfoListReq, r
 		}
 		if p, ok := data["sucess_num"]; ok {
 			tmp.SucessNum = utils.GetInt64(p)
+			SuccessCount = SuccessCount + tmp.SucessNum
 		}
 
 		if p, ok := data["arrived_num"]; ok {
@@ -73,6 +80,43 @@ func (this *SendMsgServer) GetSendMsgInfoList(req *info.GetSendMsgInfoListReq, r
 		}
 		rsp.List = append(rsp.List, tmp)
 	}
+
+	sumwhere := []bson.M{}
+
+	if req.StartTime > 0 && req.EndTime > 0 {
+		sumwhere = append(sumwhere, bson.M{"$match": bson.M{"itime": bson.M{"$gte": req.StartTime, "$lte": req.EndTime}}})
+	}
+
+	sumwhere = append(sumwhere, bson.M{
+		"$group": bson.M{
+			"_id": "null",
+			// 发送成功总数
+			"total_sucess": bson.M{
+				"$sum": "$sucess_num",
+			},
+			// 账号数量
+			"account_count": bson.M{
+				"$sum": 1,
+			},
+		},
+	})
+
+	sumRet, err1 := mgoDeal.QueryMongoSum(db, tb, sumwhere)
+
+	var totalSucess, accountCount int64
+
+	if err1 == nil {
+		if p, ok := sumRet["total_sucess"]; ok {
+			totalSucess = utils.GetInt64(p)
+		}
+
+		if p, ok := sumRet["account_count"]; ok {
+			accountCount = utils.GetInt64(p)
+		}
+	}
+
+	rsp.SuccessCount = totalSucess
+	rsp.Average = totalSucess / accountCount
 
 	go func() {
 		msgInfoList := sendmsg.GetListSendMsgInfo(bson.M{}, -1)
--- wechat/src/webface/webstru/send_msg_json.go
+++ wechat/src/webface/webstru/send_msg_json.go
@@ -3,13 +3,17 @@ package info
 type GetSendMsgInfoListReq struct {
 	Account       string `json:"account"`        //账号查询
 	AccountStatus int64  `json:"account_status"` //账号状态 1-离线 2-在线
+	StartTime     int64  `json:"start_time"`     //开始时间
+	EndTime       int64  `json:"end_time"`       //结束时间
 	Page          int64  `json:"page"`
 	Limit         int64  `json:"limit"`
 }
 
 type GetSendMsgInfoListRsp struct {
-	Total int64                     `json:"total"`
-	List  []*GetSendMsgInfoListInfo `json:"list"`
+	Total        int64                     `json:"total"`
+	List         []*GetSendMsgInfoListInfo `json:"list"`
+	SuccessCount int64                     `json:"success_count"` //发送完成总数
+	Average      int64                     `json:"average"`       //平均发送数
 }
 type GetSendMsgInfoListInfo struct {
 	Id            string `json:"id"`
